6218a9db01e5d35fee0858bdf26ef5f208a2266d
Brynster <b.abendstern@googlemail.com>
2022-03-08 11:27:27 +0000

    extract ArticleCard from ArticleList into it's own component

---
+++ src/components/Articles/ArticleCard.jsx
@@ -0,0 +1,31 @@
+import { Link } from 'react-router-dom';
+
+export default function ArticleCard({ article }) {
+  const date = new Date(article.created_at);
+
+  return (
+    <article className="article-card" key={`article-${article.article_id}`}>
+      <Link to={`/article/${article.article_id}`}>
+        <h3>{article.title}</h3>
+      </Link>
+      <h4>
+        Posted in <Link to={`/topic/${article.topic}`}>{article.topic}</Link> at{' '}
+        {`${date.toLocaleTimeString()}, ${date.getDate()}/${date.getMonth()}/${date.getFullYear()} by ${
+          article.author
+        }`}
+      </h4>
+      <p>
+        <b>{article.votes}</b> votes
+      </p>
+      <p>
+        {article.body.length > 75
+          ? `${article.body.slice(0, 72)}...`
+          : article.body}
+      </p>
+      <p>
+        {/* Could make this link to an anchor on SingleArticle page */}
+        <b>{article.comment_count}</b> comments
+      </p>
+    </article>
+  );
+}
