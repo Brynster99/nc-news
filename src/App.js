fa6978fb99646a6194dbf6df61eedde9986bb578
Brynster <b.abendstern@googlemail.com>
2022-03-09 10:35:53 +0000

    small changes to delete-comments

--- src/App.js
+++ src/App.js
@@ -1,30 +1,30 @@
-import { Routes, Route } from 'react-router-dom';
-import { useState } from 'react';
 import './App.css';
-import Header from './components/Header/Header';
+
+import { Route, Routes } from 'react-router-dom';
+
 import ArticleList from './components/Articles/ArticleList';
+import ErrorPage from './components/Misc/ErrorPage';
+import Header from './components/Header/Header';
 import Navbar from './components/Header/Navbar';
 import SingleArticle from './components/Articles/SingleArticle';
-import ErrorPage from './components/Misc/ErrorPage';
 import UserContext from './Contexts/User';
+import { useState } from 'react';
 
 export default function App() {
   const [user, setUser] = useState('jessjelly');
 
   return (
     <UserContext.Provider value={{ user, setUser }}>
-      <div className="App">
-        <Header />
-        <Navbar />
-        <main>
-          <Routes>
-            <Route path="/" element={<ArticleList />} />
-            <Route path="/topic/:topic_slug" element={<ArticleList />} />
-            <Route path="/article/:article_id" element={<SingleArticle />} />
-            <Route path="*" element={<ErrorPage />} />
-          </Routes>
-        </main>
-      </div>
+      <Header />
+      <Navbar />
+      <main>
+        <Routes>
+          <Route path="/" element={<ArticleList />} />
+          <Route path="/topic/:topic_slug" element={<ArticleList />} />
+          <Route path="/article/:article_id" element={<SingleArticle />} />
+          <Route path="*" element={<ErrorPage />} />
+        </Routes>
+      </main>
     </UserContext.Provider>
   );
 }
